215f854979bdb62fec05beb4035942d13a72e67e
DiegoBuzanello <47098158+DiegoBuzanello@users.noreply.github.com>
2019-05-28 22:24:52 -0300

    narrativa

--- assets/js/storyscript.js
+++ assets/js/storyscript.js
@@ -10,6 +10,66 @@ $('#myRightArrow').click(function() {
     }
 });
 
+$('#myRightArroww').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    nextSlide.fadeIn(300).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrowww').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    nextSlide.fadeIn(300).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrowwww').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    nextSlide.fadeIn(300).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrowwwww').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    nextSlide.fadeIn(300).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrowwwwww').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    nextSlide.fadeIn(300).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
 $('#myLeftArrow').click(function () {
     var currentSlide = $('.showFade.active');
     var backSlide = currentSlide.prev();
