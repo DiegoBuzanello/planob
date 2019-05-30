8978f61e60218e9837761474575d3dd3f7065a70
DiegoBuzanello <47098158+DiegoBuzanello@users.noreply.github.com>
2019-05-30 00:28:35 -0300

    ta quase

--- assets/js/storyscript.js
+++ assets/js/storyscript.js
@@ -2,8 +2,8 @@ $('#myRightArrow').click(function() {
     var currentSlide = $('.showFade.active');
     var nextSlide = currentSlide.next();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
 
     if (nextSlide.length == 0) {
         $('.showFade').first().fadeIn(300).addClass('active');
@@ -14,8 +14,8 @@ $('#myRightArroww').click(function() {
     var currentSlide = $('.showFade.active');
     var nextSlide = currentSlide.next();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
 
     if (nextSlide.length == 0) {
         $('.showFade').first().fadeIn(300).addClass('active');
@@ -26,8 +26,8 @@ $('#myRightArrowww').click(function() {
     var currentSlide = $('.showFade.active');
     var nextSlide = currentSlide.next();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
 
     if (nextSlide.length == 0) {
         $('.showFade').first().fadeIn(300).addClass('active');
@@ -38,8 +38,8 @@ $('#myRightArrowwww').click(function() {
     var currentSlide = $('.showFade.active');
     var nextSlide = currentSlide.next();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
 
     if (nextSlide.length == 0) {
         $('.showFade').first().fadeIn(300).addClass('active');
@@ -50,8 +50,8 @@ $('#myRightArrowwwww').click(function() {
     var currentSlide = $('.showFade.active');
     var nextSlide = currentSlide.next();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
 
     if (nextSlide.length == 0) {
         $('.showFade').first().fadeIn(300).addClass('active');
@@ -62,8 +62,56 @@ $('#myRightArrowwwwww').click(function() {
     var currentSlide = $('.showFade.active');
     var nextSlide = currentSlide.next();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrowwwwwww').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrow2').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrow3').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
+
+    if (nextSlide.length == 0) {
+        $('.showFade').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#myRightArrow4').click(function() {
+    var currentSlide = $('.showFade.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(0).removeClass('active');
+    nextSlide.fadeIn(200).addClass('active');
 
     if (nextSlide.length == 0) {
         $('.showFade').first().fadeIn(300).addClass('active');
@@ -74,8 +122,19 @@ $('#myLeftArrow').click(function () {
     var currentSlide = $('.showFade.active');
     var backSlide = currentSlide.prev();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    backSlide.fadeIn(300).addClass('active');
+    currentSlide.fadeOut(0).removeClass('active');
+    backSlide.fadeIn(200).addClass('active');
+
+    if (backSlide.length == 0) {
+        $('.showFade').last().fadeIn(300).addClass('active');
+    }
+});
+$('#myLeftArroww').click(function () {
+    var currentSlide = $('.showFade.active');
+    var backSlide = currentSlide.prev();
+
+    currentSlide.fadeOut(0).removeClass('active');
+    backSlide.fadeIn(200).addClass('active');
 
     if (backSlide.length == 0) {
         $('.showFade').last().fadeIn(300).addClass('active');
